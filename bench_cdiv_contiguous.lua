3c80d24618fbed0ef391c1ba640c782a66d97e53
jokeren <robinho364@gmail.com>
2016-12-11 13:28:40 +0800

    correct benchmarks

--- bench_cdiv_contiguous.lua
+++ bench_cdiv_contiguous.lua
@@ -6,10 +6,11 @@ torch.setdefaulttensortype('torch.FloatTensor')
 sz_single = 100000
 input1single = torch.randn(sz_single)
 input2single = torch.randn(sz_single)
+input3single = torch.randn(sz_single)
 print("contiguous float div single thread:")
 tm = torch.Timer()
 for i=1,100 do
-   input1single = input1single / input2single
+   input3single:cdiv(input1single, input2single)
 end
 print(tm:time().real)
 
@@ -17,10 +18,11 @@ print(tm:time().real)
 sz_multi = sz_single * 24
 input1multi = torch.randn(sz_multi)
 input2multi = torch.randn(sz_multi)
+input3multi = torch.randn(sz_multi)
 print("contiguous float div multi-thread:")
 tm = torch.Timer()
 for i=1,100 do
-   input1multi = input1multi / input2multi
+   input3multi:cdiv(input1multi, input2multi)
 end
 print(tm:time().real)
 
@@ -29,10 +31,11 @@ torch.setdefaulttensortype('torch.DoubleTensor')
 sz_single = 100000
 input1single = torch.randn(sz_single)
 input2single = torch.randn(sz_single)
+input3single = torch.randn(sz_single)
 print("contiguous double div single thread:")
 tm = torch.Timer()
 for i=1,100 do
-   input1single = input1single / input2single
+   input3single:cdiv(input1single, input2single)
 end
 print(tm:time().real)
 
@@ -40,9 +43,10 @@ print(tm:time().real)
 sz_multi = sz_single * 24
 input1multi = torch.randn(sz_multi)
 input2multi = torch.randn(sz_multi)
+input3multi = torch.randn(sz_multi)
 print("contiguous double div multi-thread:")
 tm = torch.Timer()
 for i=1,100 do
-   input1multi = input1multi / input2multi
+   input3multi:cdiv(input1multi, input2multi)
 end
 print(tm:time().real)
--- bench_cmul_contiguous.lua
+++ bench_cmul_contiguous.lua
@@ -6,10 +6,11 @@ torch.setdefaulttensortype('torch.FloatTensor')
 sz_single = 100000
 input1single = torch.randn(sz_single)
 input2single = torch.randn(sz_single)
+input3single = torch.randn(sz_single)
 print("contiguous float mul single thread:")
 tm = torch.Timer()
 for i=1,100 do
-   input1single = input1single * input2single
+   input3single:cmul(input1single, input2single)
 end
 print(tm:time().real)
 
@@ -17,10 +18,11 @@ print(tm:time().real)
 sz_multi = sz_single * 24
 input1multi = torch.randn(sz_multi)
 input2multi = torch.randn(sz_multi)
+input3multi = torch.randn(sz_multi)
 print("contiguous float mul multi-thread:")
 tm = torch.Timer()
 for i=1,100 do
-   input1multi = input1multi * input2multi
+   input3multi:cmul(input1multi, input2multi)
 end
 print(tm:time().real)
 
@@ -29,10 +31,11 @@ torch.setdefaulttensortype('torch.DoubleTensor')
 sz_single = 100000
 input1single = torch.randn(sz_single)
 input2single = torch.randn(sz_single)
+input3single = torch.randn(sz_single)
 print("contiguous double mul single thread:")
 tm = torch.Timer()
 for i=1,100 do
-   input1single = input1single * input2single
+   input3single:cmul(input1single, input2single)
 end
 print(tm:time().real)
 
@@ -40,10 +43,11 @@ print(tm:time().real)
 sz_multi = sz_single * 24
 input1multi = torch.randn(sz_multi)
 input2multi = torch.randn(sz_multi)
+input3multi = torch.randn(sz_multi)
 print("contiguous double mul multi-thread:")
 tm = torch.Timer()
 for i=1,100 do
-   input1multi = input1multi * input2multi
+   input3multi:cmul(input1multi, input2multi)
 end
 print(tm:time().real)
 
