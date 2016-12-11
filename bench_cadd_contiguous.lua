54b125195e55b829ccfe5d53914c6ff975379a36
jokeren <robinho364@gmail.com>
2016-12-11 11:59:44 +0800

    Separate add and cadd

--- bench_cadd_contiguous.lua
+++ bench_cadd_contiguous.lua
@@ -1,24 +1,26 @@
-# Tensor addition benchmark
+# Tensor cadd benchmark
 require("torch")
 
 torch.setdefaulttensortype('torch.FloatTensor')
 
 sz_single = 100000
 input1single = torch.randn(sz_single)
-print("contiguous float add single thread:")
+input2single = torch.randn(sz_single)
+print("contiguous float cadd single thread:")
 tm = torch.Timer()
 for i=1,100 do
-   input1single:add(1.0)
+   input1single = input1single + input2single
 end
 print(tm:time().real)
 
 --sz_single * OMP_NUM_THREADS
 sz_multi = sz_single * 24
 input1multi = torch.randn(sz_multi)
-print("contiguous float add multi-thread:")
+input2multi = torch.randn(sz_multi)
+print("contiguous float cadd multi-thread:")
 tm = torch.Timer()
 for i=1,100 do
-   input1multi:add(1.0)
+   input1multi = input1multi + input2multi 
 end
 print(tm:time().real)
 
@@ -26,19 +28,21 @@ torch.setdefaulttensortype('torch.DoubleTensor')
 
 sz_single = 100000
 input1single = torch.randn(sz_single)
-print("contiguous double add single thread:")
+input2single = torch.randn(sz_single)
+print("contiguous double cadd single thread:")
 tm = torch.Timer()
 for i=1,100 do
-   input1single:add(1.0)
+   input1single = input1single + input2single
 end
 print(tm:time().real)
 
 --sz_single * OMP_NUM_THREADS
 sz_multi = sz_single * 24
 input1multi = torch.randn(sz_multi)
-print("contiguous double add multi-thread:")
+input2multi = torch.randn(sz_multi)
+print("contiguous double cadd multi-thread:")
 tm = torch.Timer()
 for i=1,100 do
-   input1multi:add(1.0)
+   input1multi = input1multi + input2multi
 end
 print(tm:time().real)
