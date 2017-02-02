8d1c9c9f253fde19f964e7e57e0e59555da347bf
jokeren <robinho364@gmail.com>
2017-02-02 19:42:05 +0800

    refractor

--- contiguous/bench_contiguous.lua
+++ contiguous/bench_contiguous.lua
@@ -1,8 +1,7 @@
-# Tensor noncontiguous benchmark
+# Tensor contiguous benchmark
 require("torch")
 
-local initial_shape = torch.LongStorage({2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2})
-local sz = torch.pow(2, 16)
+local sz = 10000000
 
 function test_fill(input1)
    for i=1,100 do
@@ -53,77 +52,24 @@ function test_cdiv(input1, input2, input3)
 end
 
 function test_apply_cases(function_call)
-   print("read noncontiguous:")
-   input1 = torch.randn(sz):resize(initial_shape)
-   input1 = input1:narrow(15,1,1)
+   input1 = torch.randn(sz)
    tm = torch.Timer()
    function_call(input1)
    print(tm:time().real)
 end
 
 function test_apply2_cases(function_call)
-   print("read noncontiguous:")
-   input1 = torch.randn(sz):resize(initial_shape)
-   input1 = input1:narrow(15,1,1)
+   input1 = torch.randn(sz)
    input2 = input1:clone()
-   tm = torch.Timer()
-   function_call(input1, input2)
-   print(tm:time().real)
-
-   print("store noncontiguous:")
-   input2 = torch.randn(sz):resize(initial_shape)
-   input2 = input2:narrow(15,1,1)
-   input1 = input2:clone()
-   tm = torch.Timer()
-   function_call(input1, input2)
-   print(tm:time().real)
-
-   print("read noncontiguous:")
-   input1 = torch.randn(sz):resize(initial_shape)
-   input1 = input1:narrow(15,1,1)
-   input2 = torch.randn(sz):resize(initial_shape)
-   input2 = input2:narrow(15,1,1)
    tm = torch.Timer()
    function_call(input1, input2)
    print(tm:time().real)
 end
 
 function test_apply3_cases(function_call)
-   print("read one noncontiguous:")
-   input1 = torch.randn(sz):resize(initial_shape)
-   input1 = input1:narrow(15,1,1)
+   input1 = torch.randn(sz)
    input2 = input1:clone()
    input3 = input1:clone()
-   tm = torch.Timer()
-   function_call(input1, input2, input3)
-   print(tm:time().real)
-
-   print("read both noncontiguous:")
-   input1 = torch.randn(sz):resize(initial_shape)
-   input1 = input1:narrow(15,1,1)
-   input2 = torch.randn(sz):resize(initial_shape)
-   input2 = input2:narrow(15,1,1)
-   input3 = input2:clone()
-   tm = torch.Timer()
-   function_call(input1, input2, input3)
-   print(tm:time().real)
-
-   print("store noncontiguous:")
-   input3 = torch.randn(sz):resize(initial_shape)
-   input3 = input3:narrow(15,1,1)
-   input1 = input3:clone()
-   input2 = input3:clone()
-   tm = torch.Timer()
-   function_call(input1, input2, input3)
-   print(tm:time().real)
-
-   print("all noncontiguous:")
-   input1 = torch.randn(sz):resize(initial_shape)
-   input1 = input1:narrow(15,1,1)
-   input2 = torch.randn(sz):resize(initial_shape)
-   input2 = input2:narrow(15,1,1)
-   input3 = torch.randn(sz):resize(initial_shape)
-   input3 = input3:narrow(15,1,1)
    tm = torch.Timer()
    function_call(input1, input2, input3)
    print(tm:time().real)
--- transposed/bench_transposed.lua
+++ transposed/bench_transposed.lua
@@ -1,12 +1,12 @@
-# Tensor noncontiguous benchmark
+# Tensor transposed benchmark
 require("torch")
 
-local initial_shape = torch.LongStorage({2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2})
-local sz = torch.pow(2, 16)
+local initial_shape = torch.LongStorage({10000, 1000})
+local sz = 10000000
 
 function test_fill(input1)
    for i=1,100 do
-      input1:fill(0)
+      input1:fill(0);
    end
 end
 
@@ -30,32 +30,31 @@ end
 
 function test_copy(input1, input2)
    for i=1,100 do
-      input2:copy(input1)
+      input2:copy(input1);
    end
 end
 
 function test_cadd(input1, input2, input3)
    for i=1,100 do
-      input3:add(input1, input2)
+      input3:add(input1, input2);
    end
 end
 
 function test_cmul(input1, input2, input3)
    for i=1,100 do
-      input3:cmul(input1, input2)
+      input3:cmul(input1, input2);
    end
 end
 
 function test_cdiv(input1, input2, input3)
    for i=1,100 do
-      input3:cdiv(input1, input2)
+      input3:cdiv(input1, input2);
    end
 end
 
 function test_apply_cases(function_call)
    print("read noncontiguous:")
-   input1 = torch.randn(sz):resize(initial_shape)
-   input1 = input1:narrow(15,1,1)
+   input1 = torch.randn(sz):resize(initial_shape):transpose(1,2)
    tm = torch.Timer()
    function_call(input1)
    print(tm:time().real)
@@ -63,26 +62,22 @@ end
 
 function test_apply2_cases(function_call)
    print("read noncontiguous:")
-   input1 = torch.randn(sz):resize(initial_shape)
-   input1 = input1:narrow(15,1,1)
+   input1 = torch.randn(sz):resize(initial_shape):transpose(1,2)
    input2 = input1:clone()
    tm = torch.Timer()
    function_call(input1, input2)
    print(tm:time().real)
 
    print("store noncontiguous:")
-   input2 = torch.randn(sz):resize(initial_shape)
-   input2 = input2:narrow(15,1,1)
+   input2 = torch.randn(sz):resize(initial_shape):transpose(1,2)
    input1 = input2:clone()
    tm = torch.Timer()
    function_call(input1, input2)
    print(tm:time().real)
 
-   print("read noncontiguous:")
-   input1 = torch.randn(sz):resize(initial_shape)
-   input1 = input1:narrow(15,1,1)
-   input2 = torch.randn(sz):resize(initial_shape)
-   input2 = input2:narrow(15,1,1)
+   print("both noncontiguous:")
+   input1 = torch.randn(sz):resize(initial_shape):transpose(1,2)
+   input2 = torch.randn(sz):resize(initial_shape):transpose(1,2)
    tm = torch.Timer()
    function_call(input1, input2)
    print(tm:time().real)
@@ -90,8 +85,7 @@ end
 
 function test_apply3_cases(function_call)
    print("read one noncontiguous:")
-   input1 = torch.randn(sz):resize(initial_shape)
-   input1 = input1:narrow(15,1,1)
+   input1 = torch.randn(sz):resize(initial_shape):transpose(1,2)
    input2 = input1:clone()
    input3 = input1:clone()
    tm = torch.Timer()
@@ -99,18 +93,15 @@ function test_apply3_cases(function_call)
    print(tm:time().real)
 
    print("read both noncontiguous:")
-   input1 = torch.randn(sz):resize(initial_shape)
-   input1 = input1:narrow(15,1,1)
-   input2 = torch.randn(sz):resize(initial_shape)
-   input2 = input2:narrow(15,1,1)
+   input1 = torch.randn(sz):resize(initial_shape):transpose(1,2)
+   input2 = torch.randn(sz):resize(initial_shape):transpose(1,2)
    input3 = input2:clone()
    tm = torch.Timer()
    function_call(input1, input2, input3)
    print(tm:time().real)
 
    print("store noncontiguous:")
-   input3 = torch.randn(sz):resize(initial_shape)
-   input3 = input3:narrow(15,1,1)
+   input3 = torch.randn(sz):resize(initial_shape):transpose(1,2)
    input1 = input3:clone()
    input2 = input3:clone()
    tm = torch.Timer()
@@ -118,38 +109,35 @@ function test_apply3_cases(function_call)
    print(tm:time().real)
 
    print("all noncontiguous:")
-   input1 = torch.randn(sz):resize(initial_shape)
-   input1 = input1:narrow(15,1,1)
-   input2 = torch.randn(sz):resize(initial_shape)
-   input2 = input2:narrow(15,1,1)
-   input3 = torch.randn(sz):resize(initial_shape)
-   input3 = input3:narrow(15,1,1)
+   input1 = torch.randn(sz):resize(initial_shape):transpose(1,2)
+   input2 = torch.randn(sz):resize(initial_shape):transpose(1,2)
+   input3 = torch.randn(sz):resize(initial_shape):transpose(1,2)
    tm = torch.Timer()
    function_call(input1, input2, input3)
    print(tm:time().real)
 end
 
 function tester()
-   local test_apply_functions = {
-      ["fill"] = test_fill
-   }
+   --local test_apply_functions = {
+   --   ["fill"] = test_fill
+   --}
 
-   for name, func in pairs(test_apply_functions) do
-      print(name)
-      test_apply_cases(func)
-   end
+   --for name, func in pairs(test_apply_functions) do
+   --   print(name)
+   --   test_apply_cases(func)
+   --end
 
-   local test_apply2_functions = {
-      ["add"] = test_add,
-      ["mul"] = test_mul,
-      ["div"] = test_div,
-      ["copy"] = test_copy
-   }
+   --local test_apply2_functions = {
+   --   ["add"] = test_add,
+   --   ["mul"] = test_mul,
+   --   ["div"] = test_div,
+   --   ["copy"] = test_copy
+   --}
 
-   for name, func in pairs(test_apply2_functions) do
-      print(name)
-      test_apply2_cases(func)
-   end
+   --for name, func in pairs(test_apply2_functions) do
+   --   print(name)
+   --   test_apply2_cases(func)
+   --end
 
    local test_apply3_functions = {
       ["cadd"] = test_cadd,
@@ -170,3 +158,4 @@ tester()
 torch.setdefaulttensortype('torch.DoubleTensor')
 print("Double tensor tests")
 tester()
+
